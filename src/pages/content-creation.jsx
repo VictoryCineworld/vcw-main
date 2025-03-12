6f9a5bd0a73796edfafe388cab5fd3b98d99e45a
VictoryCineworld <it@victorycineworld.com>
2025-03-12 11:53:49 +0530

    Changes

--- src/pages/content-creation.jsx
+++ src/pages/content-creation.jsx
@@ -1,13 +1,13 @@
 import SEO from '@components/seo';
 import { Wrapper } from '@layout/index';
 import React from 'react';
-import ContentCreation from '@components/content-creation/ContentCreation';
+import CampaignContentCreation from '@components/service/campaign-content-creation/ccc';
 
 const index = () => {
     return (
        <Wrapper>
         <SEO pageTitle="Faime"/>
-        <ContentCreation/>
+        <CampaignContentCreation/>
        </Wrapper>
     );
 };
